4b71e50d4fbc22da7e82bc5924803575b49ac417
Gilotto <Gilotto@keemail.me>
2020-12-09 08:31:09 +0100

    Day2 part 2 finished

--- Day2.fs
+++ Day2.fs
@@ -39,7 +39,7 @@ module Solution1 =
         System.IO.File.ReadAllLines("./Puzzle2Input.txt")
 
     let parseInputString (inputString:string) =
-        inputString.Split([|' '; ':'; '-'|]) //{|1;3;a;abcde|} 
+        inputString.Split([|' '; ':'; '-'|]) //{|1;3;a;;abcde|} 
         |> fun array -> 
                     {
             lowest = int array.[0]
@@ -60,4 +60,56 @@ module Solution1 =
         |> Array.map checkNumber
         |> Array.countBy id
 
-        
+module Excercise2 = 
+    let text =
+        """
+        While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+    The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+    Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+    Given the same example list from above:
+
+    1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+    1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+    2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+    How many passwords are valid according to the new interpretation of the policies?
+        """
+module Solution2  =
+
+    type PasswordPolicy =
+        {
+            char1:char
+            char2:char
+            letter:char
+            password:string
+        }
+    
+    let puzzleInput = 
+        System.IO.File.ReadAllLines("./Puzzle2Input.txt")
+
+    let parseInputString (inputString:string) =
+        let getCharacterFromArray ((number:int), (password:string)) = password.[number - 1]
+        inputString.Split([|' '; ':'; '-'|]) //{|1;3;a;;abcde|} 
+        |> fun array -> 
+                    {
+            char1 = getCharacterFromArray (int array.[0], string array.[4] )
+            char2 = getCharacterFromArray (int array.[1], string array.[4] )
+            letter = char array.[2]
+            password = string array.[4]
+            }
+
+    let checkNumber passwordPolicy =
+        match passwordPolicy.letter with
+        |letter when letter = passwordPolicy.char1 && letter <> passwordPolicy.char2 -> true
+        |letter when letter = passwordPolicy.char2 && letter <> passwordPolicy.char1 -> true
+        |_ -> false
+
+    let solution1 =
+        puzzleInput
+        |> Array.map parseInputString
+        |> Array.map checkNumber
+        |> Array.countBy id
+
