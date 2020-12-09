32eb1c9c3e00862dc01e1844aca23900b87fc93d
Gilotto <Gilotto@keemail.me>
2020-12-09 10:50:25 +0100

    Day2 part 2

--- Day3.fs
+++ Day3.fs
@@ -69,12 +69,52 @@ module Solution1 =
     let findCharinString (index, mapString:string) =
         mapString.[(index - 1) % mapString.Length]
     
-    let solution1 = 
-        puzzleInput
+    let filterVertical slopeVertical input=
+        input
         |> Array.indexed
-        |> Array.map (fun (index,mapString) -> 1 + (index * 3), mapString) //index 0 corresponds to char 1, index 1 corresponds to char 1 + 1*3 =4 etc.
+        |> Array.filter (fun (index, mapstring) -> (index % slopeVertical) = 0)
+        |> Array.map (fun (_,relevantMapstring) -> relevantMapstring)
+
+    let solution1 input slopeHorizontal slopeVertical = 
+        input
+        |> filterVertical slopeVertical
+        |> Array.indexed
+        |> Array.map (fun (index,mapString) -> 1 + (index * slopeHorizontal), mapString) //index 0 corresponds to char 1, index 1 corresponds to char 1 + 1*3 =4 etc.
         |> Array.map findCharinString
         |> Array.map (fun char -> if char = '#' then true else false)
         |> Array.countBy id
-        |> printfn "answer = %A" 
+        |> Array.filter (fun (yesOrNo , _) -> yesOrNo)
+        |> fun array -> array.[0] |> fun ( _ , number) -> number
+        |> fun answer -> 
+            printfn "Answer = %i; for slope Hor: %i; Ver: %i " answer slopeHorizontal slopeVertical
+            answer
 
+    solution1 puzzleInput 3 1 |> ignore
+
+module Excercise2 = 
+    let text =
+        """
+--- Part Two ---
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+    Right 1, down 1.
+    Right 3, down 1. (This is the slope you already checked.)
+    Right 5, down 1.
+    Right 7, down 1.
+    Right 1, down 2.
+
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
+        """
+module Solution2 = 
+    open Solution1
+
+    let solution2 =
+        [1,1; 3,1; 5,1; 7,1; 1,2]
+        |> List.map (fun (hor,vert) -> solution1 puzzleInput hor vert |> float)
+        |> List.reduce (*)
