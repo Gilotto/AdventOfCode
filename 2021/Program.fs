bcf3d7479cf5a25318874ac1677a6a9da5ed98e0
Gilotto <Gilotto@keemail.me>
2021-12-01 14:43:53 +0100

    day1

--- 2021/Program.fs
+++ 2021/Program.fs
@@ -2,48 +2,52 @@ namespace AdventOfCode
 
 open System
 open Parser
+open Day01
 module main =
 
-    module TestValues =
-        let testValid = "ABC"
-        let testError = "BCD"
-        let testEmpty = ""
-        let testLower = "abc"
-        let testNumbers = "123"
+    // module TestValues =
+    //     let testValid = "ABC"
+    //     let testError = "BCD"
+    //     let testEmpty = ""
+    //     let testLower = "abc"
+    //     let testNumbers = "123"
 
-        let tests = [testValid;testError;testEmpty;testLower;testNumbers]
+    //     let tests = [testValid;testError;testEmpty;testLower;testNumbers]
 
-    module ParseSettings =
-        let parseA = pchar 'A'
-        let parseB = pchar 'B'
-        let parseAthenB = parseA .>>. parseB
-        let parseAorB = parseA <|> parseB
+    // module ParseSettings =
+    //     let parseA = pchar 'A'
+    //     let parseB = pchar 'B'
+    //     let parseAthenB = parseA .>>. parseB
+    //     let parseAorB = parseA <|> parseB
         
-        let parseLowercase =
-            anyOf ['a'..'z']
+    //     let parseLowercase =
+    //         anyOf ['a'..'z']
         
-        let parseDigit =
-            anyOf ['0'..'9']
+    //     let parseDigit =
+    //         anyOf ['0'..'9']
 
-        let parseThreeDigits =
-            parseDigit .>>. parseDigit .>>. parseDigit
+    //     let parseThreeDigits =
+    //         parseDigit .>>. parseDigit .>>. parseDigit
 
-        let parseThreeDigitsAsStr =
-            parseThreeDigits |>> fun ((c1, c2), c3) -> String [| c1; c2; c3|]
+    //     let parseThreeDigitsAsStr =
+    //         parseThreeDigits |>> fun ((c1, c2), c3) -> String [| c1; c2; c3|]
 
-        let parseThreeDigitsAsInt =
-            mapP int parseThreeDigitsAsStr
+    //     let parseThreeDigitsAsInt =
+    //         mapP int parseThreeDigitsAsStr
 
-        let parsers = [ 'A'; 'B'; 'C'] |> List.map pchar
+    //     let parsers = [ 'A'; 'B'; 'C'] |> List.map pchar
 
-        let combined = sequence parsers
+    //     let combined = sequence parsers
 
-        let parseABC = pstring "ABC"
-        let parserSettings = parseABC
+    //     let parseABC = pstring "ABC"
+    //     let parserSettings = parseABC
+
 
     [<EntryPoint>]
     let main argv =
-        TestValues.tests
-        |> List.map (ParseSettings.parserSettings |> run)
-        |> List.iter (printfn "%A")
+        printfn "%i" solution01_part02
+        
+        // TestValues.tests
+        // |> List.map (ParseSettings.parserSettings |> run)
+        // |> List.iter (printfn "%A")
         0 // return an integer exit code
