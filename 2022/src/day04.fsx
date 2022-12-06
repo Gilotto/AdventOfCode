585612ae344d9baaed72bf29f9ff7298ce13b1a5
Gilotto <Gilotto@keemail.me>
2022-12-06 10:32:37 +0100

    part 2 solved

--- 2022/src/day04.fsx
+++ 2022/src/day04.fsx
@@ -47,6 +47,40 @@ In how many assignment pairs does one range fully contain the other?
 let data = System.IO.File.ReadAllLines "../AdventOfCode/2022/input/day04.txt"
 
 data
-|> Array.toList
-|> List.map (fun s -> s.Split [|',';'-'|] |> Array.map int |> (fun a -> if (a.[0] >= a.[2] && a.[1] <= a.[3]) || (a.[0] <= a.[2] && a.[1] >= a.[3]) then 1 else 0))
-|> List.sum
+|> Array.map (fun s -> 
+    s.Split [|',';'-'|] 
+    |> Array.map int 
+    |> (fun a -> 
+        if (
+            a.[0] >= a.[2] && a.[1] <= a.[3]) || 
+            (a.[0] <= a.[2] && a.[1] >= a.[3]) 
+        then 1 
+        else 0))
+|> Array.sum
+
+let day4_part2 = "--- Part Two ---
+
+It seems like there is still quite a bit of duplicate work planned. Instead, the Elves would like to know the number of pairs that overlap at all.
+
+In the above example, the first two pairs (2-4,6-8 and 2-3,4-5) don't overlap, while the remaining four pairs (5-7,7-9, 2-8,3-7, 6-6,4-6, and 2-6,4-8) do overlap:
+
+    5-7,7-9 overlaps in a single section, 7.
+    2-8,3-7 overlaps all of the sections 3 through 7.
+    6-6,4-6 overlaps in a single section, 6.
+    2-6,4-8 overlaps in sections 4, 5, and 6.
+
+So, in this example, the number of overlapping assignment pairs is 4.
+
+In how many assignment pairs do the ranges overlap?
+"
+
+data
+|> Array.map (fun s -> 
+    s.Split [|',';'-'|] 
+    |> Array.map int 
+    |> (fun a -> 
+        [a.[0] .. a.[1]]@ [a.[2] .. a.[3]] 
+        |> List.groupBy id
+        |> List.exists (fun (_,li) -> li.Length = 2 )) 
+)
+|> Array.countBy id
